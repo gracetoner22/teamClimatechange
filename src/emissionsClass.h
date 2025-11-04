989dcfa50915db2c422f2cd9b0cec75dea428c79
gracetoner22 <snappyginger22@gmail.com>
2025-11-03 19:31:19 -0500

    Add files via upload

---
+++ src/emissionsClass.h
@@ -0,0 +1,33 @@
+#ifndef EMISSIONS_SOURCE_H
+#define EMISSIONS_SOURCE_H
+
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+struct naryNode {
+    std::string state;
+    double emission;
+    std::unordered_map<std::string, naryNode*> childrenMap;
+
+    naryNode(const std::string& state, double emission = -1)
+        : state(state), emission(emission) {}
+};
+
+class naryTree {
+public:
+    naryNode* root;
+    naryTree();
+
+    void addState(const std::vector<std::string>& states);
+    void addYear(const std::string& stateName, const std::vector<std::string>& years);
+    void addEmission(const std::string& stateName, const std::string& year, double emission);
+    void printEmission(const std::string& stateName, const std::string& year);
+    void printTree(naryNode* node = nullptr, int depth = 0);
+    void buildTreeFromFile(const std::string& filename);
+    double getEmission(const std::string& state, const std::string& year) const ;
+    void deleteSubtree(naryNode* node);
+    ~naryTree();
+};
+
+#endif
